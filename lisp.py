9bb10858ee5df31c0553628a45cd1bee0227b0c0
Moshe <moshberm@gmail.com>
2014-03-29 22:35:28 -0400

    Finished Q1 and began on Q2.

--- lisp.py
+++ lisp.py
@@ -1,14 +1,24 @@
-#    The homework 2.5 for Professor Zhou's CISC 3150 class at Brooklyn College.
+'''
 
+  Moshe Berman
+  Professor Zhou
+  CISC 3150
+  Spring 2014
 
-#    Utilities to mimic Lisp and Scheme.
+  Assignment 2.5
+
+'''
+
+'''    Utilities to mimic Lisp and Scheme. '''
 
 def car(lst):
     return lst[0]
     
 def cdr(lst):
     return lst[1:]
-   
+  
+''' Q1. Write in Python, the following functions on Lisp-style lists. '''
+
 #    Sums the elements in a list
 def sumList(lst):
     if not lst:
@@ -17,7 +27,7 @@ def sumList(lst):
         return car(lst) + sumList(cdr(lst))
     
  
- #    Checks if x is in a list
+ #    tests if x is an element in lst
 def member(x, lst):
   if not lst:
       return False
@@ -31,6 +41,23 @@ def duplicate(lst):
     else:
         return [car(lst), car(lst)] + duplicate(cdr(lst))   
 
+# Checks if an element is the prefix.
+def is_prefix(pre, lst):
+  if not pre:
+    return True
+  elif not car(pre) == car(lst):
+    return False
+  else:
+    return is_prefix(cdr(pre), cdr(lst))
+
+''' Q2. Consider sets represented as Lisp-style lists. Write the following functions. ''' 
+
+# tests if subs is a subset of s
+def subset(sub, s):
+
+
+''' Test the functions... '''
+
 #    Tests 
 print "Testing sumList(lst)" 
 print sumList([1, 2, 3]) #    6
@@ -43,5 +70,9 @@ print member(1, [1,2,3]) #    True
 print "Testing duplicate(lst)"
 print duplicate([])
 print duplicate([8, 10, 30, 52])
+
+print "Testing is_prefix(pre, lst)"
+print is_prefix([1,2], [1,2,3,4])
+print is_prefix([1,3], [1,2,3,4])
 
  # RUN: http://ideone.com/wkla46
